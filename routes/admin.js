0d296f6e69b979f056755b670443d4c8b4235445
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2021-02-02 00:11:56 +0530

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -13,6 +13,8 @@ require("dotenv").config();
 router.use(cookieParser());
 router.use(bodyParser.json());
 
+const url = "mongodb+srv://satyaprasadbehara:Fdwe6cYnwFMERYMC@cluster0.efor9.mongodb.net/CustomerRelationshipManagement?retryWrites=true&w=majority";
+
 router.route("/login").post(async (req, res) => {
     const {
         email,
@@ -26,90 +28,86 @@ router.route("/login").post(async (req, res) => {
         errors.push("password field is required !!");
     }
     if (errors.length === 0) {
-        try {
-            let client = await mongoClient.connect(url, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }); //connect to db
-            let db = client.db("CustomerRelationshipManagement"); //db name
-            let user = db.collection("users"); //collection name
-            user.findOne({
-                    email: email,
-                },
-                (err, User) => {
-                    if (err) {
-                        return res.json({
-                            error: "something went wrong",
-                        });
-                    }
+        let client = await mongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }); //connect to db
+        let db = client.db("CustomerRelationshipManagement"); //db name
+        let user = db.collection("users"); //collection name
+        await user.findOne({
+                email: email,
+            },
+            (err, User) => {
+                console.log(User)
+                if (err) {
+                    console.log(err);
+                    return res.json({
+                        error: err,
+                    });
+                }
+                if (User === null) {
+                    return res.json({error: `No registered user found with ${email}`})
+                } else if (User.userType !== "admin") {
+                    return res.json({
+                        error: User.userType +
+                            "  will not have permission to access admin portal",
+                    });
+                } else {
                     if (User === null) {
-                        errors.push("No registered user found with " + email);
-                    } else if (User.userType !== "admin") {
                         return res.json({
-                            error: User.userType + "  will not have permission to access admin portal"
+                            message: "No registered user found with " + email,
                         });
-                    }
-                    if (errors.length === 0) {
-                        if (User === null) {
-                            return res.json({
-                                message: "No registered user found with " + email,
-                            });
-                        } else {
-                            let usertype = User.userType;
-                            let name = User.fname + " " + User.lname;
-                            if (User.verified == true) {
-                                let passwordMatched = bcrypt.compare(
-                                    password,
-                                    User.password,
-                                    result.password
-                                );
-                                if (passwordMatched == true) {
-                                    //if matched
-                                    let token = EncodeToken(email);
-                                    res
-                                        .cookie("jwt", token, {
-                                            maxAge: 1000000,
-                                            // httpOnly: true,
-                                            // secure: true,
-                                        })
-                                        .cookie("userType", usertype, {
-                                            maxAge: 1000000,
-                                            // httpOnly: true,
-                                            // secure: true,
-                                        })
-                                        .cookie("user", email, {
-                                            maxAge: 1000000,
-                                            // httpOnly: true,
-                                            // secure: true,
-                                        })
-                                        .json({
-                                            message: "Hello " + name + " , you are successfully logged in...", //if credentials matched,
-                                        });
-                                } else {
-                                    return res.json({
-                                        error: "Invalid Credentials..", //if the credentials were not matching
+                    } else {
+                        let usertype = User.userType;
+                        let name = User.fname + " " + User.lname;
+                        if (User.verified == true) {
+                            let passwordMatched = bcrypt.compare(
+                                password,
+                                User.password,
+                                result.password
+                            );
+                            if (passwordMatched == true) {
+                                //if matched
+                                let token = EncodeToken(email);
+                                res
+                                    .cookie("jwt", token, {
+                                        maxAge: 1000000,
+                                        // httpOnly: true,
+                                        // secure: true,
+                                    })
+                                    .cookie("userType", usertype, {
+                                        maxAge: 1000000,
+                                        // httpOnly: true,
+                                        // secure: true,
+                                    })
+                                    .cookie("user", email, {
+                                        maxAge: 1000000,
+                                        // httpOnly: true,
+                                        // secure: true,
+                                    })
+                                    .json({
+                                        message: "Hello " +
+                                            name +
+                                            " , you are successfully logged in...", //if credentials matched,
                                     });
-                                }
                             } else {
                                 return res.json({
-                                    error: "User Identity not verified..",
+                                    error: "Invalid Credentials..", //if the credentials were not matching
                                 });
                             }
+                        } else {
+                            return res.json({
+                                error: "User Identity not verified..",
+                            });
                         }
-                    } else {
-                        return res.json({
-                            error: errors,
-                        });
                     }
                 }
-            );
-            client.close()
-        } catch (err) {
-            console.log(err)
-            return res.json({
-                error: 'something went wrong'
-            });
-        }
+            }
+        );
+    } else {
+        return res.json({
+            error: errors
+        });
     }
 });
 
